523fff4adf79d03afb71855adae6d8d0eea933a6
Jason Schlesinger <jschlesi+n@gmail.com>
2026-03-17 15:44:46 +0900

    Added LaTeX environment

---
+++ writing/latex/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "DevShell for Writing in English";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , ...
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      lib = {
+        mkWritingShell = { extraPkgs ? [ ], extraShellHook ? "" }:
+          pkgs.mkShell {
+            # uses TexLive, Pandoc and Harper
+            # TexLive: https://nixos.wiki/wiki/TexLive
+            buildInputs = with pkgs; [
+              harper
+              texlab
+              pandoc
+              texlive.combined.scheme-medium
+            ] ++ extraPkgs;
+
+            shellHook = ''
+              export NIX_PROVIDED_LSPS="harper_ls,taxlab"
+              # run additional shellHook from child
+              ${extraShellHook}
+
+              echo "🐠 Entering Fish shell..."
+              exec fish
+            '';
+          };
+      };
+      # if you accidentally run nix develop in the repo
+      devShells.default = pkgs.mkShell {
+        buildInputs = [ pkgs.hello ];
+        shellHook = "echo 'This is the parent repo. Use this as a flake input!'";
+      };
+    });
+}
